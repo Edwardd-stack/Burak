494579566ff7d7919fb4a91b0077a04fa089b0de
Edward <sardormadgoziyev@gmail.com>
2026-09-22 16:44:39 +0900

    taskM: get Square Numbers

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,17 @@
-console.log("Hello Task!");
+console.log("\n M - Task \n");
+
+// M-TASK
+function SquareNumbers(numbers: number[]) {
+  let result: { number: number; square: number }[] = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    let num = numbers[i];
+    let sq = num * num;
+
+    result.push({ number: num, square: sq });
+  }
+
+  return result;
+}
+
+console.log(SquareNumbers([5, 7, 9]));
