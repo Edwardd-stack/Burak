9264e0e3b0a5a2999aadd50f8aeba77f32ba8d18
Edward <sardormadgoziyev@gmail.com>
2026-09-24 17:59:59 +0900

    feat: develop member schema model

--- src/train.ts
+++ src/train.ts
@@ -1,17 +1,43 @@
-console.log("\n M - Task \n");
+console.log("\n >> N - Task << \n");
 
 // M-TASK
-function SquareNumbers(numbers: number[]) {
-  let result: { number: number; square: number }[] = [];
+// function SquareNumbers(numbers: number[]) {
+//   let result: { number: number; square: number }[] = [];
 
-  for (let i = 0; i < numbers.length; i++) {
-    let num = numbers[i];
-    let sq = num * num;
+//   for (let i = 0; i < numbers.length; i++) {
+//     let num = numbers[i];
+//     let sq = num * num;
 
-    result.push({ number: num, square: sq });
-  }
+//     result.push({ number: num, square: sq });
+//   }
 
-  return result;
+//   return result;
+// }
+
+// console.log(SquareNumbers([5, 7, 9]));
+
+// function palindromCheck(word: string): boolean {
+//   let reversed = "";
+//   let i = word.length - 1;
+//   while (i >= 0) {
+//     reversed = reversed + word[i];
+//     i--;
+//   }
+//   return word === reversed;
+// }
+// console.log(palindromCheck("DaD"));
+// console.log(palindromCheck("2002"));
+
+// function palindromeCheck(str: string): boolean {
+//   return str.split("").reverse().join("") === str;
+// }
+
+// ***** N task ***** //
+function palindromCheck(a: string) {
+  const reverseStr = a.split("").reverse().join("");
+  return reverseStr === a;
 }
 
-console.log(SquareNumbers([5, 7, 9]));
+console.log(palindromCheck("dad"));
+console.log(palindromCheck("son"));
+console.log("");
