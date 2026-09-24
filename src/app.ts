7c5f8871c5fc9d8c60628ad9afb89e6980cb81b4
Edward <sardormadgoziyev@gmail.com>
2026-09-24 15:47:04 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /** 1-ENTRANCE **/
 const app = express();
 console.log(__dirname);
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Traditional API uchun xizmat qiladigan (Middleware DP)
+app.use(express.json()); // Rest API sifatida kelayotgan json datalarni o'tkazishga ruhsat beradi (Middleware DP) | json => object |
 
 /** 2-SESSIONS **/
 
 /** 3-VIEWS **/
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // Traditional API larni ishga tushurish uchun ejs frameworkdan foydalandik
 
 /** 4-ROUTERS **/
+app.use("/", router);
 
 export default app;
