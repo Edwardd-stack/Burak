74793e6e5788ecb9622331c661660b56b6b78d75
Edward <sardormadgoziyev@gmail.com>
2026-09-26 02:28:26 +0900

    feat: create project standards

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,15 @@ import express from "express";
 import path from "path";
 import router from "./router";
 import routerAdmin from "./routerAdmin";
+import morgan from "morgan";
+import { MORGAN_FORMAT } from "./libs/config";
 
 /** 1-ENTRANCE **/
 const app = express();
-console.log(__dirname);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true })); // Traditional API uchun xizmat qiladigan (Middleware DP)
 app.use(express.json()); // Rest API sifatida kelayotgan json datalarni o'tkazishga ruhsat beradi (Middleware DP) | json => object |
+app.use(morgan(MORGAN_FORMAT));
 
 /** 2-SESSIONS **/
 
